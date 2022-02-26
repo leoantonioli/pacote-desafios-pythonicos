1046d7b112c43e3988924dd4a9a6c98e09598897
leoantonioli <100436769+leoantonioli@users.noreply.github.com>
2022-02-25 22:07:54 -0300

    Update 09_front_x.py

--- 09_front_x.py
+++ 09_front_x.py
@@ -12,8 +12,16 @@ antes de combina-las.
 """
 
 def front_x(words):
-    # +++ SUA SOLUÇÃO +++
-    return
+    listaX = [] # Cria lista vazia para palavras com X
+    lista2 = [] # Cria lista vazia pro resto
+    for i in range(len(words)): #  Cria índice para percorrer lista
+        if words[i][0] == "x": # Se a palavra da vez começa com X...
+            listaX.append(words[i]) # Coloca a palavra na Lista X
+        else:
+            lista2.append(words[i]) # Senão coloca na Lista 2
+    listaX.sort() # Ordena as listas
+    lista2.sort()
+    return listaX + lista2
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
